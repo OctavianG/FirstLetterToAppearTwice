6bcadb2f7a516f1cfdb7b71d1c2d7909e1c91f47
OctavianG <octavian7.g@gmail.com>
2023-07-13 20:32:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        
+
         System.out.println(repeatedCharacter(s));
     }
 }
